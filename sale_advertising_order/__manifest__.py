f2e29fa1124a62eed12c8f3bb7541ccc6049bcad
Deepa Venkatesh <deepa4lok@yahoo.com>
2024-12-03 12:57:31 +0530

    [384] Findings SAO & SOL: Translations

--- sale_advertising_order/__manifest__.py
+++ sale_advertising_order/__manifest__.py
@@ -22,7 +22,7 @@
 ##############################################################################
 {
     'name': 'Sale Advertising Order',
-    'version': '16.0.4.6',
+    'version': '16.0.4.7',
     'category': 'Sale',
     'description': """
 This module allows you to use Sales Management to run your advertising sales
