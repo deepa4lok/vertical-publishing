5e21d400a61f4be6eed715982ca1eaa7a9647358
Deepa Venkatesh <deepa4lok@yahoo.com>
2024-12-03 10:48:06 +0530

    [384] Findings SAO & SOL: nsm cleanup

--- sale_advertising_order/__manifest__.py
+++ sale_advertising_order/__manifest__.py
@@ -22,7 +22,7 @@
 ##############################################################################
 {
     'name': 'Sale Advertising Order',
-    'version': '16.0.4.5',
+    'version': '16.0.4.6',
     'category': 'Sale',
     'description': """
 This module allows you to use Sales Management to run your advertising sales
