bf5789ce75854579b8b5a0c328685e31c67fcbb7
Deepa Venkatesh <deepa4lok@yahoo.com>
2024-12-31 13:42:42 +0530

    [479] Multiline: Fixes

--- sale_advertising_order/__manifest__.py
+++ sale_advertising_order/__manifest__.py
@@ -22,7 +22,7 @@
 ##############################################################################
 {
     'name': 'Sale Advertising Order',
-    'version': '16.0.5.0',
+    'version': '16.0.6.0',
     'category': 'Sale',
     'description': """
 This module allows you to use Sales Management to run your advertising sales
