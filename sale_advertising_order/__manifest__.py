c345183272354dda786a3c300db480296ccac3e8
Deepa Venkatesh <deepa4lok@yahoo.com>
2025-01-22 11:30:26 +0530

    [473] Invoice: validity dates

--- sale_advertising_order/__manifest__.py
+++ sale_advertising_order/__manifest__.py
@@ -22,7 +22,7 @@
 ##############################################################################
 {
     'name': 'Sale Advertising Order',
-    'version': '16.0.8.3',
+    'version': '16.0.8.4',
     'category': 'Sale',
     'description': """
 This module allows you to use Sales Management to run your advertising sales
