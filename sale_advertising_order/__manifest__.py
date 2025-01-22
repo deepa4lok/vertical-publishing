ff837f736cb11656abcd732427488a6c50a8904d
Deepa Venkatesh <deepa4lok@yahoo.com>
2025-01-22 09:46:17 +0530

    [544] Invoice: Customer code

--- sale_advertising_order/__manifest__.py
+++ sale_advertising_order/__manifest__.py
@@ -22,7 +22,7 @@
 ##############################################################################
 {
     'name': 'Sale Advertising Order',
-    'version': '16.0.8.0',
+    'version': '16.0.8.1',
     'category': 'Sale',
     'description': """
 This module allows you to use Sales Management to run your advertising sales
